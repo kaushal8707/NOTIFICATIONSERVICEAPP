f5349f7a28bdf709958e2eddcc484edc3cf3ca20
kaushal8707 <kaushal.singh029@gmail.com>
2020-08-11 23:20:51 +0530

    Initial commit

---
+++ src/main/java/com/always/learner/NOTIFICATIONSERVICEAPP/NotificationserviceappApplication.java
@@ -0,0 +1,13 @@
+package com.always.learner.NOTIFICATIONSERVICEAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationserviceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationserviceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/always/learner/NOTIFICATIONSERVICEAPP/NotificationserviceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.NOTIFICATIONSERVICEAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
